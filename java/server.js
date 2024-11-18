9a9416fc2f53162d76ce42e6cf0bde70cba96c13
manurecchi <manurecchi03@gmail.com>
2024-11-18 20:17:44 -0300

    hice cambios

--- java/server.js
+++ java/server.js
@@ -6,13 +6,13 @@ const port = 3000;
 
 // Configuración de la conexión a la base de datos
 const dbConfig = {
-    user: 'tu_usuario',
-    password: 'tu_contraseña',
-    server: 'localhost',
-    database: 'Barberia',
-    options: {
-        encrypt: false,
-        trustServerCertificate: true,
+user: 'tu_usuario',
+password: 'tu_contraseña',
+server: 'localhost',
+ database: 'Barberia',
+ options: {
+     encrypt: false,
+ trustServerCertificate: true,
     },
 };
 
@@ -26,19 +26,19 @@ async function conectarBaseDatos() {
     }
 }
 
-// Ruta para obtener la lista de clientes
+//  obtener la lista de clientes
 app.get('/clientes', async (req, res) => {
     try {
-        const result = await sql.query('SELECT * FROM Clientes');
-        res.json(result.recordset);
+      const result = await sql.query('SELECT * FROM Clientes');
+  res.json(result.recordset);
     } catch (err) {
-        console.error('Error al obtener los clientes:', err);
-        res.status(500).send('Error al obtener los clientes');
+ console.error('Error al obtener los clientes:', err);
+  res.status(500).send('Error al obtener los clientes');
     }
 });
 
 // Iniciar el servidor y conectar a la base de datos
 app.listen(port, () => {
-    console.log(`Servidor corriendo en http://localhost:${port}`);
-    conectarBaseDatos();
+ console.log(`Servidor corriendo en http://localhost:${port}`);
+ conectarBaseDatos();
 });
